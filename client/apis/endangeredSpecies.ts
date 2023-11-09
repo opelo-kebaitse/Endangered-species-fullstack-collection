74342b1ec158e3ed5b6c5a56443fe107f3a48eda
Opelo Kebaitse <opelopatricia1@gmail.com>
2023-11-09 18:55:50 +1300

    Create a models file and update other files accordingly

--- client/apis/endangeredSpecies.ts
+++ client/apis/endangeredSpecies.ts
@@ -1,8 +1,9 @@
 import request from 'superagent'
+import { EndangeredSpecies } from '../../models/endangeredSpecies'
 
 const rootUrl = '/api/v1'
 
 export async function getEndangeredSpecies() {
   const res = await request.get(rootUrl + '/species')
-  return res.body.species
+  return res.body.species as EndangeredSpecies[]
 }
---
+++ models/endangeredSpecies.ts
@@ -0,0 +1,5 @@
+export interface EndangeredSpecies{
+id: number
+name: string
+population: number
+}
